d15ef8621e6600e31f98224a3671d08aeedaf1d2
huzhengnan <373502234@qq.com>
2025-10-23 10:51:38 +0800

    1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,19 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4602201282246605"
           crossOrigin="anonymous"
         />
+        {/* Microsoft Clarity 追踪脚本 */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tugpwdwq8g");
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
